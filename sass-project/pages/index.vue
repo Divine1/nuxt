67481413b0e34832ef0b361e293c9cee00612273
divine <divinecutler@gmail.com>
2018-07-07 08:38:08 +0530

    sass setup

---
+++ sass-project/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h3>container index</h3>
+    <h4>war</h4>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+
+@import "../assets/css/main.scss";
+
+h4{
+  color:$mycolor;
+}
+</style>
